a18d0e53bb2a6d4d8bd90ff2fed57f40a34f01de
Leonardo Andrade <leonardoh.deandrade@gmail.com>
2025-10-30 22:14:22 -0300

    feat: create SearchResults page with routing
    Add SearchResults page that:
    - Displays search results in responsive grid
    - Reads query from URL search params (?q=query)
    - Shows loading skeleton during search
    - Displays empty state when no results found
    - Shows result count when movies found
    - Integrates with MovieCard and MoviePreviewModal
    - Includes localStorage favorites persistence
    - Add /search route to App routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import MyList from './pages/MyList'
+import SearchResults from './pages/SearchResults'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/my-list" element={<MyList />} />
+      <Route path="/search" element={<SearchResults />} />
     </Routes>
   )
 }
