4dd7e150cf74cee78b31cf5802899ea07e2ea39a
Leonardo Andrade <leonardoh.deandrade@gmail.com>
2025-11-17 20:21:16 -0300

    feat: protect routes with ProtectedRoute component
    - Wrap all content routes with ProtectedRoute
    - Set /profiles route to not require profile selection
    - Require profile selection for /, /movies, /tv-shows, /my-list, /search, /watch, /profiles/manage
    - Keep /login as public route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router'
+import { ProtectedRoute } from './components/route'
 
 const Home = lazy(() => import('./pages/Home'))
 const Login = lazy(() => import('./pages/Login'))
@@ -15,15 +16,73 @@ function App() {
   return (
     <Suspense fallback={<div className="min-h-screen bg-black" />}>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profiles" element={<ProfileSelector />} />
-        <Route path="/profiles/manage" element={<ManageProfiles />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/tv-shows" element={<TvShows />} />
-        <Route path="/my-list" element={<MyList />} />
-        <Route path="/search" element={<SearchResults />} />
-        <Route path="/watch/:id" element={<Watch />} />
+
+        <Route
+          path="/profiles"
+          element={
+            <ProtectedRoute requireProfile={false}>
+              <ProfileSelector />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/movies"
+          element={
+            <ProtectedRoute>
+              <Movies />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/tv-shows"
+          element={
+            <ProtectedRoute>
+              <TvShows />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-list"
+          element={
+            <ProtectedRoute>
+              <MyList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute>
+              <SearchResults />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/watch/:id"
+          element={
+            <ProtectedRoute>
+              <Watch />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profiles/manage"
+          element={
+            <ProtectedRoute>
+              <ManageProfiles />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Suspense>
   )
